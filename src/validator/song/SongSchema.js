a5c1267b0f3a53586222fff001abca5fc83a7b91
kevin raydafa <kevinroy201104@gmail.com>
2025-11-10 16:03:13 +0700

    perbaikan Joi

--- src/validator/song/SongSchema.js
+++ src/validator/song/SongSchema.js
@@ -2,10 +2,10 @@ const Joi = require ('joi');
 
 const SongPayloadSchema = Joi.object({
     title: Joi.string().required(),
-    year: Joi.number().required(),
+    year: Joi.number().integer().required(),
     genre: Joi.string().required(),
     performer: Joi.string().required(),
-    duration: Joi.number(),
+    duration: Joi.number().integer().required(),
     albumId: Joi.string(), 
 });
 
